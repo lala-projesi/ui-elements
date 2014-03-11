edd6760632ece2d98d5e99854d0085f1d16a9af0
Hijadelsol <gunesakcay@gmail.com>
2014-03-12 01:58:58 +0200

    update app.rb

--- app.rb
+++ app.rb
@@ -4,21 +4,9 @@ Bundler.require :default
 class Sinatra::Base
   configure do  
     set :slim, {html5: true}	
-  end
-   
+  end 
+
   get '/?' do
     slim :index
-  end
-  
-  get '/dashboard' do
-    slim :dashboard
-  end 
-
-  get '/fatura' do
-    slim :fatura
-  end 
-
-  get '/rapor' do
-    slim :rapor
   end
 end
